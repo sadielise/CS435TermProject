348b7b9b3e52315185c433bbd4f0a87e8ee34466
Chad <ceitel@live.com>
2017-04-18 17:41:50 -0600

    clean data

--- src/Mapper1.java
+++ src/Mapper1.java
@@ -15,18 +15,21 @@ public class Mapper1 extends Mapper<LongWritable, Text, Text, Text>{
 
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 
-		int institutionIndex = 0;
-		int institutionNameIndex = 1;
+		int institutionIndex = 2; //OPEID 6-digit
+		int institutionNameIndex = 3;
 		int stateIndex = 5;
 		int menIndex = 33;
 		int womenIndex = 34;
 		int firstDegreeIndex = 61;
 		int lastDegreeIndex = 98;
+		
+		int firstCostNoIncomeIndex = 316;
+		int lastCostNoIncomeIndex = 319;
+		
 		int firstIncomeIndex = 320;
 		int lastIncomeIndex = 339;
-		int meanEarningsIndex = 1629;
-		int yearIndex = 1729;
-		
+		//int meanEarningsIndex = 1638;
+		int yearIndex = 1738;
 		
 		//split line
 		String[] values = value.toString().split(",");
@@ -35,40 +38,61 @@ public class Mapper1 extends Mapper<LongWritable, Text, Text, Text>{
 		
 		////KEY
 		//state new index: 0
-		toWrite.add(values[stateIndex]);
+		toWrite.add(new String(values[stateIndex]));
 		//years new index: 1
-		toWrite.add(values[yearIndex]);
+		toWrite.add(new String(values[yearIndex]));
 		
 		////VALUE
 		//menOnly new index: 2
-		toWrite.add(values[menIndex]);
+		toWrite.add(new String(values[menIndex]));
 		//womenOnly new index: 3
-		toWrite.add(values[womenIndex]);
+		toWrite.add(new String(values[womenIndex]));
 		//meanEarnings new index: 4
-		toWrite.add(values[meanEarningsIndex]);
+		//toWrite.add(new String(values[meanEarningsIndex]));
 		//institutionID new index: 5
-		toWrite.add(values[institutionIndex]);
+		toWrite.add(new String(values[institutionIndex]));
 		//institutionName new index: 6
-		toWrite.add(values[institutionNameIndex]);
+		toWrite.add(new String(values[institutionNameIndex]));
 		//fieldsOfStudy new index: 7-44
 		for(int i = firstDegreeIndex; i<=lastDegreeIndex; i++){
-			toWrite.add(values[i]);
+			toWrite.add(new String(values[i]));
 		}
-		//income new index: 45-64
+		//cost by income bracket new index: 45-64
+		int count = 0;
 		for(int i = firstIncomeIndex; i<=lastIncomeIndex; i++){
-			toWrite.add(values[i]);
+			if(values[i]!="NULL" && count<4){
+				toWrite.add(new String(values[i]));
+				count++;
+			}
 		}
 		
-		String newKey = toWrite.get(0) + "," + toWrite.get(1) + ",";
-		String newVal = toWrite.get(2);
-		for(int i=3; i<toWrite.size(); i++){
-			newVal+=newVal+","+toWrite.get(i);
+		//cost no income if income values not available
+		if(count == 0){
+			for(int i = firstCostNoIncomeIndex; i<= lastCostNoIncomeIndex; i++){
+				if(values[i]!="NULL"){
+					toWrite.add(new String(values[i]));
+					toWrite.add(new String(values[i]));
+					toWrite.add(new String(values[i]));
+					toWrite.add(new String(values[i]));
+					count++;
+					break;
+				}
+			}
 		}
 		
-		//new size SHOULD == 65;
-		
-		if(toWrite.size() == 65){
-			for(int i=0; i< toWrite.size(); i++){
+		if(count!=0){
+			String newKey = "2000,";
+			
+					
+					
+			String newVal =	new String(toWrite.get(0));
+			for(int i=1; i<toWrite.size(); i++){
+				newVal = newVal+","+toWrite.get(i);
+			}
+			
+			//new size SHOULD == 64;
+			
+			if(toWrite.size() != 0){
 				context.write(new Text(newKey), new Text(newVal));
 			}
 		}
--- src/Reducer1.java
+++ src/Reducer1.java
@@ -1,36 +1,20 @@
 import java.io.IOException;
-
-//import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
-public class Reducer1 extends Reducer<Text,Text,Text,Text>{
+public class Reducer1 extends Reducer<Text,Text,NullWritable,Text>{
 	
 	/*
 	 * Input: (state,years,necessary fields separated by commas)
 	 * Output: (state,years,necessary fields separated by commas)
 	 */
 	
-	private MultipleOutputs<Text, Text> output;
-	
-	@Override
-	public void setup(Context context){
-		output = new MultipleOutputs<Text, Text>(context);
-	}
-	
-	@Override
-	protected void cleanup(Context context) throws IOException, InterruptedException{
-		output.close();
-	}
-	
 	public void reduce(Text key, Iterable<Text> values, Context context) throws
 	IOException, InterruptedException {
 
 		for (Text value : values) {
-			output.write("job1output", key, value);
-		}
-		
-		
+			context.write(NullWritable.get(), new Text(key.toString() + value.toString()));
+		}	
 	}
 }
